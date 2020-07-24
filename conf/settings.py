ab199cc9f61e4e0573d0e36e2bb0aeb4a77138fb
Pere <pere@perepicornell.com>
2020-07-24 11:06:12 +0200

    Adding root templates folder to settings

--- conf/settings.py
+++ conf/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
