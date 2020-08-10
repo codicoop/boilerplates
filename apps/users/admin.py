6e38e44c77f4278ba73dec408fb278760882d2b0
Pere <pere@perepicornell.com>
2020-08-10 14:10:30 +0200

    Flake8 fixes

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -10,7 +10,10 @@ class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput
+    )
 
     class Meta:
         model = MyUser
