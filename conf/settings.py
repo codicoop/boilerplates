555de34d3eb0ee043daafd620574b87b61639ad1
Pere <pere@perepicornell.com>
2020-08-10 17:23:47 +0200

    Adding config for grappelli and whitenoise. Added config for putting assets in /assets

--- conf/settings.py
+++ conf/settings.py
@@ -52,10 +52,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Application definition
 INSTALLED_APPS = [
+    'apps.users',
     'django.contrib.postgres',
     'django.contrib.admin',
+    'grappelli',  # Place before contrib.admin
     'django.contrib.auth',
-    'apps.users',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -67,6 +68,7 @@ AUTH_USER_MODEL = 'users.MyUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,5 +132,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
