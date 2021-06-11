31c7a9f55988d837f34ee30a8c5a0f0033979e47
Pere <pere@perepicornell.com>
2021-06-11 12:38:02 +0200

    Configurant default id a BigAuto

--- cities/apps.py
+++ cities/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class CitiesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = 'apps.cities'
     verbose_name = "Ciutats i poblacions"
